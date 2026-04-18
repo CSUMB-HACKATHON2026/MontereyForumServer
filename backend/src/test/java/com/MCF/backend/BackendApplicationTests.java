6de6bf2b5a5a8a64f7b7d9177b62d64ea8d76bfb
Rberjaoui <Rhberjaoui@gmail.com>
2026-04-18 16:04:03 -0700

    SpringBoot added

---
+++ backend/src/test/java/com/MCF/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MCF.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
